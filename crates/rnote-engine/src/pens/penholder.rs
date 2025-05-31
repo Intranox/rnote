20a5f504ea909634cde21102b31fd13645bb5d06
Doublonmousse <115779707+Doublonmousse@users.noreply.github.com>
2025-05-31 14:39:40 +0200

    comment on redraw

--- crates/rnote-engine/src/pens/penholder.rs
+++ crates/rnote-engine/src/pens/penholder.rs
@@ -213,6 +213,9 @@ impl PenHolder {
         // so probably the actual pen handling path is quick enough
         // but the async redraw makes it hang
         // --> make it false for now
+        // actually needed for penholder draws
+        // A compromise could be to separate in the redraw the actual part we want to redraw and the
+        // rest ?
         // issue : the partial rendering phase (already done) scales with the nof strokes ?
 
         (event_result.propagate, widget_flags)
